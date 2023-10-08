1d6d722c7aa77c88870b10a150c6e0efcefd5efc
jane-alam-zayn <141549917+jane-alam-zayn@users.noreply.github.com>
2023-10-08 13:24:19 +0600

    Add files via upload

---
+++ Exp_1/E11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name:MD JANE ALOM ZAYN\n");
+    printf("Phone: 01878133891\n");
+    printf("Email: u2108035@student.cuet.ac.bd\n");
+    printf("Address:Cumilla\n");
+
+}
---
+++ Exp_1/L1.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+
+{
+
+    printf("Name:MD JANE ALOM ZAYN \n");
+
+    printf("ID: 2108035\n");
+    printf("Dept: ETE\n");
+
+    printf("Address: CUET");
+
+}
